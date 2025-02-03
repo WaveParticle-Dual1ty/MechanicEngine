74ed44f02151e27c28cb96f6dd96841732534b48
Laplace <142323522+WaveParticle-Dual1ty@users.noreply.github.com>
2025-02-03 20:24:35 +0800

    add EmptyApp

---
+++ Source/EmptyApp/EmptyApp.cpp
@@ -0,0 +1,22 @@
+﻿#include "EmptyApp.h"
+#include <memory>
+#include <iostream>
+#include <filesystem>
+#include "MechanicEngine/Include/Core/Core.h"
+#include "MechanicEngine/Include/Application/EntryPoint.h"
+#include "MechanicEngine/Include/Application/Application.h"
+
+std::unique_ptr<ME::Application> ME::CreateApplication()
+{
+    return std::make_unique<EmptyApp>();
+}
+
+EmptyApp::EmptyApp()
+    : Application({"EmptyApp"})
+{
+}
+
+bool EmptyApp::OnEngineInit()
+{
+    return true;
+}
---
+++ Source/EmptyApp/EmptyApp.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include <iostream>
+#include "MechanicEngine/Include/Core/Core.h"
+#include "MechanicEngine/Include/Application/EntryPoint.h"
+#include "MechanicEngine/Include/Application/Application.h"
+
+class EmptyApp : public ME::Application
+{
+public:
+    EmptyApp();
+
+private:
+    virtual bool OnEngineInit() override;
+};
